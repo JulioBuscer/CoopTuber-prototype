9401e7e0c07402b04ff93a3a01ab06ef68da012c
JulioBuscer <juliobuscer@gmail.com>
2025-04-21 16:45:48 -0600

    Add Avatar component with dynamic image rendering based on props
    - Implemented Avatar component in Avatar.tsx
    - Added logic to determine image path based on eyesClosed and mouthOpen props
    - Included default characterId handling
    - Styled avatar container for consistent display
    - Added talking.png image to avatars for mouth open state

--- src/types.d.ts
+++ src/types.d.ts
@@ -1,3 +1,10 @@
 interface HTMLVideoElement {
     captureStream?(frameRate?: number): MediaStream;
 }
+
+type blendshapesCategory = {
+    index: number,
+    score: number;
+    categoryName: string;
+    displayName: string
+}
