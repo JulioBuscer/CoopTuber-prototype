f442c89aa4ac9cdc5797f4dca9f2c92ba1d4be72
JulioBuscer <juliobuscer@gmail.com>
2025-04-22 07:06:32 -0600

    Add Player 2 actions

--- src/components/WebcamViewer.tsx
+++ src/components/WebcamViewer.tsx
@@ -1,6 +1,7 @@
 import Avatar from "./Avatar";
 import { createSignal, onCleanup, onMount } from "solid-js";
 import { FaceLandmarkDetector } from "../lib/FaceLandmarker";
+import Score from "./Score";
 
 type BlendshapesCategory = {
     categoryName: string;
@@ -8,8 +9,19 @@ type BlendshapesCategory = {
 };
 
 const WebcamViewer = () => {
-    const [eyesClosed, setEyesClosed] = createSignal(false);
-    const [mouthOpen, setMouthOpen] = createSignal(false);
+    const [eyesClosedP1, setEyesClosedP1] = createSignal(false);
+    const [mouthOpenP1, setMouthOpenP1] = createSignal(false);
+
+    const [eyesClosedP2, setEyesClosedP2] = createSignal(false);
+    const [mouthOpenP2, setMouthOpenP2] = createSignal(false);
+
+    const [eyeBlinkLeftScoreP1, setEyeBlinkLeftScoreP1] = createSignal(0);
+    const [eyeBlinkRightScoreP1, setEyeBlinkRightScoreP1] = createSignal(0);
+    const [jawOpenScoreP1, setJawOpenScoreP1] = createSignal(0);
+
+    const [eyeBlinkLeftScoreP2, setEyeBlinkLeftScoreP2] = createSignal(0);
+    const [eyeBlinkRightScoreP2, setEyeBlinkRightScoreP2] = createSignal(0);
+    const [jawOpenScoreP2, setJawOpenScoreP2] = createSignal(0);
 
     let videoRef: HTMLVideoElement | undefined;
     let canvasRef: HTMLCanvasElement | undefined;
@@ -25,15 +37,15 @@ const WebcamViewer = () => {
     onMount(async () => {
         try {
             // Solicitar acceso a la cámara
-            /*const stream = await navigator.mediaDevices.getUserMedia({
+            const stream = await navigator.mediaDevices.getUserMedia({
                 video: {
                     width: 640, //Ancho deseado
                     height: 480, //Alto deseado
                     facingMode: "user" // usar la camara frontal
                 }
-            });*/
+            });
             //Asignar el stream al elemento video
-            //videoRef.srcObject = stream;
+            videoRef!.srcObject = stream;
             //setVideoStream(stream);
 
             // Usar video estático en lugar de webcam
@@ -41,8 +53,8 @@ const WebcamViewer = () => {
             video.onerror = (e) => console.error("Video error:", e);
 
             // Forzar tamaño de canvas y video
-            video.width = 640;
-            video.height = 480;
+            video.width = 1920;
+            video.height = 1080;
             const canvas = canvasRef!;
             canvas.width = video.width;
             canvas.height = video.height;
@@ -52,10 +64,10 @@ const WebcamViewer = () => {
                 left: "0",
             });
 
-            video.src = "/video.mp4";
-            video.loop = true;
-            video.muted = true;
-            await video.play();
+            //video.src = "/video.mp4";
+            //video.loop = true;
+            //video.muted = true;
+            //await video.play();
 
             const landmarkDetector = new FaceLandmarkDetector(canvas);
             await landmarkDetector.initialize();
@@ -69,21 +81,44 @@ const WebcamViewer = () => {
                     if (
                         results &&
                         results.faceLandmarks &&
-                        results.faceBlendshapes &&
-                        results.faceBlendshapes[0]
+                        results.faceBlendshapes
                     ) {
-                        const blendshapes = results.faceBlendshapes[0];
-                        const findScore = (name: string) =>
-                            blendshapes.categories.find(
-                                (c: BlendshapesCategory) => c.categoryName === name
-                            )?.score ?? 0;
 
-                        const eyeBlinkLeft = findScore("eyeBlinkLeft");
-                        const eyeBlinkRight = findScore("eyeBlinkRight");
-                        const jawOpen = findScore("jawOpen");
+                        if (results.faceBlendshapes[0]) {
+                            const blendshapes = results.faceBlendshapes[0];
+                            const findScore = (name: string) =>
+                                blendshapes.categories.find(
+                                    (c: BlendshapesCategory) => c.categoryName === name
+                                )?.score ?? 0;
 
-                        setEyesClosed((eyeBlinkLeft + eyeBlinkRight) / 2 > 0.5);
-                        setMouthOpen(jawOpen > 0.3);
+                            const eyeBlinkLeft = findScore("eyeBlinkLeft");
+                            const eyeBlinkRight = findScore("eyeBlinkRight");
+                            const jawOpen = findScore("jawOpen");
+                            setEyeBlinkLeftScoreP1(eyeBlinkLeft);
+                            setEyeBlinkRightScoreP1(eyeBlinkRight);
+                            setJawOpenScoreP1(jawOpen);
+
+                            setEyesClosedP1((eyeBlinkLeft + eyeBlinkRight) / 2 > 0.4);
+                            setMouthOpenP1(jawOpen > 0.09);
+                        }
+                        if (results.faceBlendshapes[1]) {
+                            const blendshapes = results.faceBlendshapes[1];
+                            const findScore = (name: string) =>
+                                blendshapes.categories.find(
+                                    (c: BlendshapesCategory) => c.categoryName === name
+                                )?.score ?? 0;
+
+                            const eyeBlinkLeft = findScore("eyeBlinkLeft");
+                            const eyeBlinkRight = findScore("eyeBlinkRight");
+                            const jawOpen = findScore("jawOpen");
+                            setEyeBlinkLeftScoreP2(eyeBlinkLeft);
+                            setEyeBlinkRightScoreP2(eyeBlinkRight);
+                            setJawOpenScoreP2(jawOpen);
+
+                            setEyesClosedP2((eyeBlinkLeft + eyeBlinkRight) / 2 > 0.4);
+                            setMouthOpenP2(jawOpen > 0.09);
+                        }
+
                         det.drawResults(results);
                     } else {
                         console.log("No se detectaron landmarks");
@@ -106,6 +141,7 @@ const WebcamViewer = () => {
             style={{
                 display: "flex",
                 gap: "20px",
+                "flex-direction": "column",
                 "justify-content": "center",
                 "align-items": "center",
                 padding: "20px",
@@ -118,16 +154,51 @@ const WebcamViewer = () => {
                 <video ref={el => (videoRef = el!)} autoplay muted playsinline />
                 <canvas
                     ref={el => (canvasRef = el!)}
-                    width={640}
-                    height={480}
+                    width={1920}
+                    height={1080}
                     style="width: 100%; height: 100%; object-fit: cover;"
                 />
             </div>
-            <Avatar
-                eyesClosed={eyesClosed()}
-                mouthOpen={mouthOpen()}
-                characterId="face1"
-            />
+
+            <div
+                style={{
+                    display: "flex",
+                    gap: "20px",
+                }}>
+                <Score
+                    faceNumber={1}
+                    eyeBlinkLeftScore={eyeBlinkLeftScoreP1()}
+                    eyeBlinkRightScore={eyeBlinkRightScoreP1()}
+                    jawOpenScore={jawOpenScoreP1()} />
+                <Score
+                    faceNumber={2}
+                    eyeBlinkLeftScore={eyeBlinkLeftScoreP2()}
+                    eyeBlinkRightScore={eyeBlinkRightScoreP2()}
+                    jawOpenScore={jawOpenScoreP2()} />
+            </div>
+            <div
+                style={{
+                    display: "flex",
+                    gap: "20px",
+                    'width': '1920px',
+                    'height': 'auto',
+                    'max-width': '100%',
+                    'aspect-ratio': '16/9',
+                    'background-color': '#00FF00',
+                    padding: '5px',
+                }}>
+                <Avatar
+                    eyesClosed={eyesClosedP1()}
+                    mouthOpen={mouthOpenP1()}
+                    characterId="face1"
+                />
+                <Avatar
+                    eyesClosed={eyesClosedP2()}
+                    mouthOpen={mouthOpenP2()}
+                    characterId="face2"
+                />
+            </div>
+
         </div>
     );
 };
