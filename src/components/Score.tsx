add4768606be74e2dd644a48c5645c76d9772312
JulioBuscer <juliobuscer@gmail.com>
2025-05-03 13:49:10 -0600

    refactor: update player state management, enhance Avatar and ImageAvatar components, and improve type definitions for better clarity

--- src/components/Score.tsx
+++ src/components/Score.tsx
@@ -1,35 +1,39 @@
-import { Component, Signal } from "solid-js";
-import usePlayers, { players } from "../data/signals/player";
+import { Component, createMemo } from "solid-js";
+import { AvatarConfig, AvatarState } from "../data/types/avatar";
+import { playersStates } from "../data/signals/player";
 
 interface ScoreProps {
-    faceName: string;
+    player: AvatarConfig
 }
 
-const Score: Component<ScoreProps> = (props) => {
-    const player = players().find(p => p.characterId === props.faceName);
-    if (!player) {
-        return <> No se encontro el jugador {props.faceName}</>;
+const Score: Component<ScoreProps> = ({ player }) => {
+    const state = createMemo(() => {
+        return playersStates().find(p => p.characterId === player.characterId);
+    });
+
+    if (!state()) {
+        return <> Cargando estado para {player.characterId}...</>;
     };
 
-    const { rateEyesClosed, rateMouthOpen, eyeBlinkLeftScore, eyeBlinkRightScore, jawOpenScore } = player;
+    const { rateEyesClosed, rateMouthOpen } = player;
 
     return (
         <div >
             <div>
                 <h4>Eye Blink Left:</h4>
-                <p>{eyeBlinkLeftScore.toFixed(2)}</p>
-                <progress max={rateEyesClosed} value={eyeBlinkLeftScore} > {eyeBlinkLeftScore} </progress>
+                <p>{state()!.eyeBlinkLeftScore.toFixed(2)}</p>
+                <progress max={rateEyesClosed} value={state()!.eyeBlinkLeftScore} > {state()!.eyeBlinkLeftScore} </progress>
 
             </div>
             <div>
                 <h4>Eye Blink Right:</h4>
-                <p>{eyeBlinkRightScore.toFixed(2)}</p>
-                <progress max={rateEyesClosed} value={eyeBlinkRightScore} > {eyeBlinkRightScore} </progress>
+                <p>{state()!.eyeBlinkRightScore.toFixed(2)}</p>
+                <progress max={rateEyesClosed} value={state()!.eyeBlinkRightScore} > {state()!.eyeBlinkRightScore} </progress>
             </div>
             <div>
                 <h4>Jaw Open:</h4>
-                <p>{jawOpenScore.toFixed(2)}</p>
-                <progress max={rateMouthOpen} value={jawOpenScore} > {jawOpenScore} </progress>
+                <p>{state()!.jawOpenScore.toFixed(2)}</p>
+                <progress max={rateMouthOpen} value={state()!.jawOpenScore} > {state()!.jawOpenScore} </progress>
             </div>
         </div>
     );
